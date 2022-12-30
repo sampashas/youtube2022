0820f2e433a9421c9d6a1fb69b3c06efdad36b6c
Pavel S <sampashass@gmail.com>
2022-12-30 23:45:33 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Product from './pages/Product/Product'
+import Products from './pages/Products/Products'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/products/:id',
+    element: <Products />,
+  },
+  {
+    path: '/product/:id',
+    element: <Product />,
+  },
+])
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
