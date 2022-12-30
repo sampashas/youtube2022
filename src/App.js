908adf81c3b71a7bd6efdaef381666cf3d470f18
Pavel S <sampashass@gmail.com>
2022-12-30 23:49:44 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+// pages
 import Home from './pages/Home/Home'
 import Product from './pages/Product/Product'
 import Products from './pages/Products/Products'
+// components
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/products/:id',
-    element: <Products />,
-  },
-  {
-    path: '/product/:id',
-    element: <Product />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/products/:id',
+        element: <Products />,
+      },
+      {
+        path: '/product/:id',
+        element: <Product />,
+      },
+    ],
   },
 ])
 
