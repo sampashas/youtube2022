7efbe6ed6daf583d8c221a3624a3303cce8efa1c
Pavel S <sampashass@gmail.com>
2022-12-31 15:22:06 +0700

    header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import SearchIcon from '@mui/icons-material/Search'
@@ -8,13 +9,85 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 
 function Navbar() {
   return (
-    <div className="">
+    <div className="flex gap-2 w-full items-center justify-between px-6 py-4">
       {' '}
-      <KeyboardArrowDownIcon />
-      <SearchIcon />
-      <PersonOutlineIcon />
-      <FavoriteBorderIcon />
-      <ShoppingCartOutlinedIcon />
+      {/* left */}
+      <div className="flex w-[33.3vw] gap-4">
+        {/* lang + price */}
+        <div className="flex">
+          <span>Eng</span>
+          <KeyboardArrowDownIcon />
+        </div>
+        <div className="flex">
+          <span>USD</span>
+          <KeyboardArrowDownIcon />
+        </div>
+        {/* links */}
+        <ul className="flex gap-6">
+          <li>
+            <Link to="/products/1">
+              <span>Men</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/products/2">
+              <span>Women</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/products/3">
+              <span>Children</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/products/4">
+              <span>Accessories</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      {/* center */}
+      <Link to="/">
+        <span className="text-xl">App/Market</span>
+      </Link>
+      {/* right */}
+      <div className="flex w-[33.3vw] justify-end gap-6">
+        {/* links */}
+        <ul className="flex gap-6">
+          <li>
+            <Link to="/">
+              <span>Homepage</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">
+              <span>About</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/contacts">
+              <span>Contacts</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/stores">
+              <span>Stores</span>
+            </Link>
+          </li>
+        </ul>
+        {/* interface */}
+        <div className="flex gap-4">
+          <SearchIcon />
+          <PersonOutlineIcon />
+          <FavoriteBorderIcon />
+          <div className="flex relative">
+            <ShoppingCartOutlinedIcon />
+            <div className="absolute left-3 bottom-3 w-5 h-5 bg-[var(--primary)] rounded-full flex justify-center items-center">
+              <span className="text-xs text-white">0</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
